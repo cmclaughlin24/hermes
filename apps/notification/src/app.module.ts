b382f587fbf28375fd9a06ce6cf7b4d09807c414
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-17 12:49:02 -0600

    Add Postgres Database & Sequelize ORM

--- apps/notification/src/app.module.ts
+++ apps/notification/src/app.module.ts
@@ -2,6 +2,7 @@ import * as Joi from '@hapi/joi';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ConsumerModule } from './consumers/consumer.module';
 import { NotificationJobModule } from './resources/notification-job/notification-job.module';
 import { NotificationModule } from './resources/notification/notification.module';
@@ -25,6 +26,21 @@ import { NotificationModule } from './resources/notification/notification.module
         TWILIO_AUTH_TOKEN: Joi.required(),
         TWILIO_PHONE_NUMBER: Joi.required(),
       }),
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false
+      })
     }),
     BullModule.forRootAsync({
       imports: [ConfigModule],
