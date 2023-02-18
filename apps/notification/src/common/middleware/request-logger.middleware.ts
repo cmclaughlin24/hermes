76cddf74e4ca46366c0e25cd20a0c94187d2bfb8
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-18 10:57:42 -0600

    RequestLoggerMiddleware Implementation

--- apps/notification/src/common/middleware/request-logger.middleware.ts
+++ apps/notification/src/common/middleware/request-logger.middleware.ts
@@ -1,10 +1,23 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(RequestLoggerMiddleware.name);
 
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
+    const { method, originalUrl } = req;
+    const startTime = new Date().getTime();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const responseTime = new Date().getTime() - startTime;
+
+      this.logger.log(
+        `Method=${method} URL=${originalUrl} Status=${statusCode} ResponseTime=${responseTime}ms`,
+      );
+    });
+
     next();
   }
 }
