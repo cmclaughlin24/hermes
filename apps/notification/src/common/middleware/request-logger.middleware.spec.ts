db4b0438946de7f87ecc8ae902ddfc4d256897d9
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-20 15:04:22 -0600

    Request Logger Middleware Unit Test Set-up

--- apps/notification/src/common/middleware/request-logger.middleware.spec.ts
+++ apps/notification/src/common/middleware/request-logger.middleware.spec.ts
@@ -4,4 +4,18 @@ describe('RequestLoggerMiddleware', () => {
   it('should be defined', () => {
     expect(new RequestLoggerMiddleware()).toBeDefined();
   });
+
+  describe('use()', () => {
+    let requestLogger: RequestLoggerMiddleware;
+
+    beforeEach(() => {
+      requestLogger = new RequestLoggerMiddleware();
+    });
+
+    afterEach(() => {
+      requestLogger = null;
+    });
+
+    it.todo('investigate how to unit test middlewares');
+  });
 });
