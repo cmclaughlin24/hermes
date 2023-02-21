1cee96e4e511beb25a918225b2821190e274bb6e
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-21 06:02:25 -0600

    Update package.json w/build scripts for both services

--- apps/notification/src/config/swagger.config.ts
+++ apps/notification/src/config/swagger.config.ts
@@ -9,7 +9,7 @@ export function setupSwaggerDocument(app: INestApplication) {
     .setVersion(packageJson.version)
     .setDescription(
       'The Notification Service implements a Queue design pattern by utilizing the @nestjs/bull package. This ' +
-        'package provides a layer of abstraction for the Bull library, a robust queue system based on Redis. ' +
+        'package provides a layer of abstraction for the Bull library, a robust message queue system based on Redis. ' +
         'It allows for more a reliable communication channel between the Notification Service and other services ' +
         'by persisting job data and application state so that task handling can be easily restarted. The Notification ' +
         'Service listens for jobs to be added to the "notification" queue and continues to process each until all ' +
