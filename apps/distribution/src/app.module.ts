cdb4ac1b6a13f01b787714f49464de56ff20dda8
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-22 05:49:40 -0600

    Connect Distribution Service to database

--- apps/distribution/src/app.module.ts
+++ apps/distribution/src/app.module.ts
@@ -2,7 +2,9 @@ import * as Joi from '@hapi/joi';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { CommonModule } from './common/common.module';
+import { databaseFactory } from './config/database.config';
 import { ResourcesModule } from './resources/resources.module';
 import { SubscribersModule } from './subscribers/subscribers.module';
 
@@ -12,9 +14,20 @@ import { SubscribersModule } from './subscribers/subscribers.module';
       ignoreEnvFile: true,
       isGlobal: true,
       validationSchema: Joi.object({
+        API_KEY: Joi.required(),
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.required(),
+        DB_PASSWORD: Joi.required(),
+        DB_NAME: Joi.required(),
         REDIS_HOST: Joi.required(),
         REDIS_PORT: Joi.number().required(),
       }),
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: databaseFactory,
     }),
     BullModule.forRootAsync({
       imports: [ConfigModule],
--- apps/distribution/src/config/swagger.config.ts
+++ apps/distribution/src/config/swagger.config.ts
@@ -1,10 +1,18 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { API_KEY_HEADER } from '@notification/common';
 
 export function setupSwaggerDocument(app: INestApplication) {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Distribution Service Documentation')
-    .addTag('Order')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        in: 'header',
+        name: API_KEY_HEADER,
+      },
+      'ApiKeyAuth',
+    )
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
