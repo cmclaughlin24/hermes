251bfa46f24e9534f5777db8981d1da8495b2f2f
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-22 11:38:52 -0600

    EventHandlerController Set-up

--- apps/distribution/src/event-handlers/default-event-handler/default-event-handler.controller.ts
+++ apps/distribution/src/event-handlers/default-event-handler/default-event-handler.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Controller } from '@nestjs/common';
+import { Queue } from 'bull';
 
 export function createDefaultEventHandlerController(name: string) {
-  @Controller(name)
+  @Controller()
   class DefaultEventHandlerController {
-    @Get()
-    success() {
-      return `Successfully made request to ${name}`;
+    static readonly prefix = name;
+
+    constructor(
+      @InjectQueue('notification') public readonly notificationQueue: Queue,
+    ) {}
+
+    listen() {
+      // Fixme: Use @EventPattern(name) to handle events.
     }
   }
 
--- apps/distribution/src/event-handlers/event-handlers.module.ts
+++ apps/distribution/src/event-handlers/event-handlers.module.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bull';
 import { DynamicModule, Module } from '@nestjs/common';
 import { createDefaultEventHandlerController } from './default-event-handler/default-event-handler.controller';
 
@@ -8,6 +9,8 @@ export interface EventHandlersModuleOptions {
 @Module({})
 export class EventHandlersModule {
   static forRoot(options: EventHandlersModuleOptions): DynamicModule {
+    // Note: Dynamically create EventHandlerControllers to listen for different events. Allows
+    //       for new channels to use default notification rules or custom rules.
     const controllers = [];
 
     for (const event of options.events) {
@@ -23,6 +26,11 @@ export class EventHandlersModule {
 
     return {
       module: EventHandlersModule,
+      imports: [
+        BullModule.registerQueue({
+          name: 'notification',
+        }),
+      ],
       controllers: controllers,
     };
   }
