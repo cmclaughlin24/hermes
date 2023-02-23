26f0687622b4ec1ae1b2f5ad2fd69e43d37f2f68
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-02-23 05:41:48 -0600

    Remove @nest/microservices dependency

--- apps/distribution/src/main.ts
+++ apps/distribution/src/main.ts
@@ -1,22 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { setupSwaggerDocument } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const microservice = await app.connectMicroservice({
-    transport: Transport.REDIS,
-    options: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-    },
-  });
   const port = process.env.PORT || 3001;
 
   setupSwaggerDocument(app);
 
-  await app.startAllMicroservices();
   await app.listen(port);
 }
 bootstrap();
