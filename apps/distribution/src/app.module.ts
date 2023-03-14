e4187d01fc3a9a1d82f3022436eee430d5bdc52f
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-03-14 07:21:40 -0500

    Install and Set-up @golevelup/nestjs-rabbitmq

--- apps/distribution/src/app.module.ts
+++ apps/distribution/src/app.module.ts
@@ -1,3 +1,4 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import * as Joi from '@hapi/joi';
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
@@ -6,6 +7,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { CommonModule } from './common/common.module';
 import { bullFactory } from './config/bull.config';
 import { databaseFactory } from './config/database.config';
+import { rabbitmqFactory } from './config/rabbitmq.config';
 import { ConsumersModule } from './consumers/consumers.module';
 import { ResourcesModule } from './resources/resources.module';
 
@@ -21,6 +23,7 @@ import { ResourcesModule } from './resources/resources.module';
         DB_USERNAME: Joi.required(),
         DB_PASSWORD: Joi.required(),
         DB_NAME: Joi.required(),
+        RABBITMQ_URI: Joi.required(),
         REDIS_HOST: Joi.required(),
         REDIS_PORT: Joi.number().required(),
         RETRY_ATTEMPTS: Joi.number().required(),
@@ -36,6 +39,11 @@ import { ResourcesModule } from './resources/resources.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: bullFactory,
+    }),
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: rabbitmqFactory,
     }),
     ResourcesModule,
     CommonModule,
---
+++ apps/distribution/src/config/rabbitmq.config.ts
@@ -0,0 +1,11 @@
+import { RabbitMQConfig } from '@golevelup/nestjs-rabbitmq';
+import { ConfigService } from '@nestjs/config';
+
+export async function rabbitmqFactory(
+  configService: ConfigService,
+): Promise<RabbitMQConfig> {
+  return {
+    uri: configService.get('RABBITMQ_URI'),
+    exchanges: [],
+  };
+}
