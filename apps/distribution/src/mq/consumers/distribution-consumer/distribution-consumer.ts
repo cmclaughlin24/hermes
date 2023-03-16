25750722695e097ed7ed21fe8bd57e3246a96213
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-03-16 14:44:11 -0500

    Implement example of dead letter retry queue

--- apps/distribution/src/mq/consumers/distribution-consumer/distribution-consumer.ts
+++ apps/distribution/src/mq/consumers/distribution-consumer/distribution-consumer.ts
@@ -1,6 +1,7 @@
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Nack, RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ConsumeMessage } from 'amqplib';
 import { Queue } from 'bullmq';
 import { SubscriptionFilterService } from '../../../common/providers/subscription-filter/subscription-filter.service';
@@ -14,15 +15,47 @@ export class DistributionConsumer {
   constructor(
     @InjectQueue(process.env.BULLMQ_NOTIFICATION_QUEUE)
     private readonly notificationQueue: Queue,
+    private readonly configService: ConfigService,
     private readonly distributionRuleService: DistributionRuleService,
     private readonly subscriptionFilterService: SubscriptionFilterService,
     private readonly subscriptionMemberService: SubscriptionMemberService,
   ) {}
 
-  @RabbitSubscribe({ queue: process.env.RABBITMQ_DISTRIBUTION_QUEUE })
+  @RabbitSubscribe({
+    queue: process.env.RABBITMQ_DISTRIBUTION_QUEUE,
+    queueOptions: {
+      // Note: If RABBITMQ_DL_EXCHANGE and RABBITMQ_DISTRIBUTION_DL_ROUTING_KEY are null/undefined,
+      //       message will not be retried.
+      deadLetterExchange: process.env.RABBITMQ_DL_EXCHANGE,
+      deadLetterRoutingKey: process.env.RABBITMQ_DISTRIBUTION_DL_ROUTING_KEY,
+    },
+  })
   async subscribe(message: {}, amqpMsg: ConsumeMessage) {
     const logPrefix = this._createLogPrefix(this.subscribe.name, '');
     this.logger.log(`${logPrefix} ${JSON.stringify(message)}`);
+
+    if (this._shouldRetry(amqpMsg)) {
+      return new Nack(false);
+    }
+  }
+
+  /**
+   * Yields true if a message has not exceeded the max retry attempts or false
+   * otherwise.
+   * @param {ConsumeMessage} amqpMsg
+   * @returns {boolean}
+   */
+  private _shouldRetry(amqpMsg: ConsumeMessage): boolean {
+    const headers = amqpMsg.properties.headers;
+    const queueHeader = headers['x-death']?.find(
+      (header) =>
+        header.queue === this.configService.get('RABBITMQ_DISTRIBUTION_QUEUE'),
+    );
+
+    return (
+      !queueHeader ||
+      queueHeader.count < this.configService.get('RETRY_ATTEMPTS')
+    );
   }
 
   /**
