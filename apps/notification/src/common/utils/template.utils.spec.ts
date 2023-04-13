0f021e818c121aa6e57352e4ae03fd789f6a8a60
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-04-13 10:49:19 -0500

    Notification Service | Rename context to data in template utils to avoid confusion

--- apps/notification/src/common/utils/template.utils.spec.ts
+++ apps/notification/src/common/utils/template.utils.spec.ts
@@ -15,24 +15,24 @@ describe('template.utils.ts', () => {
       expect(result).toBe(text);
     });
 
-    it('should yield a string where the key patterns have been replaced with values from the context argument', () => {
+    it('should yield a string where the key patterns have been replaced with values from the data argument', () => {
       // Arrange.
       const text =
         'Lorem ipsum dolor {{hello}} amet, consectetur adipiscing {{world.value}}.';
-      const context = { hello: 'veniam', world: { value: 'dolore' } };
-      const expectedText = `Lorem ipsum dolor ${context.hello} amet, consectetur adipiscing ${context.world.value}.`;
+      const data = { hello: 'veniam', world: { value: 'dolore' } };
+      const expectedText = `Lorem ipsum dolor ${data.hello} amet, consectetur adipiscing ${data.world.value}.`;
 
       // Act.
-      const result = compileTextTemplate(text, context);
+      const result = compileTextTemplate(text, data);
 
       // Assert.
       expect(result).toBe(expectedText);
     });
 
-    it('should throw an error if the context is null/undefined if the key pattern is present', () => {
+    it('should throw an error if the data is null/undefined if the key pattern is present', () => {
       // Arrange.
       const expectedError = new Error(
-        'Invalid Argument: context cannot be null/undefined if the key pattern is present',
+        'Invalid Argument: data cannot be null/undefined if the key pattern is present',
       );
 
       // Act.
@@ -42,57 +42,57 @@ describe('template.utils.ts', () => {
       expect(func).toThrow(expectedError);
     });
 
-    it('should throw an error if the context is not an object (string)', () => {
+    it('should throw an error if the data is not an object (string)', () => {
       // Arrange.
-      const context = 'test';
+      const data = 'test';
       const expectedError = new Error(
-        `Invalid Argument: context must be type 'object', recieved ${typeof context}`,
+        `Invalid Argument: data must be type 'object', recieved ${typeof data}`,
       );
 
       // Act.
-      const func = compileTextTemplate.bind(null, text, context);
+      const func = compileTextTemplate.bind(null, text, data);
 
       // Assert.
       expect(func).toThrow(expectedError);
     });
 
-    it('should throw an error if the context is not an object (number)', () => {
+    it('should throw an error if the data is not an object (number)', () => {
       // Arrange.
-      const context = 1;
+      const data = 1;
       const expectedError = new Error(
-        `Invalid Argument: context must be type 'object', recieved ${typeof context}`,
+        `Invalid Argument: data must be type 'object', recieved ${typeof data}`,
       );
 
       // Act.
-      const func = compileTextTemplate.bind(null, text, context);
+      const func = compileTextTemplate.bind(null, text, data);
 
       // Assert.
       expect(func).toThrow(expectedError);
     });
 
-    it('should throw an error if the context is not an object (boolean)', () => {
+    it('should throw an error if the data is not an object (boolean)', () => {
       // Arrange.
-      const context = true;
+      const data = true;
       const expectedError = new Error(
-        `Invalid Argument: context must be type 'object', recieved ${typeof context}`,
+        `Invalid Argument: data must be type 'object', recieved ${typeof data}`,
       );
 
       // Act.
-      const func = compileTextTemplate.bind(null, text, context);
+      const func = compileTextTemplate.bind(null, text, data);
 
       // Assert.
       expect(func).toThrow(expectedError);
     });
 
-    it('should throw an error if the context is an array', () => {
+    it('should throw an error if the data is an array', () => {
       // Arrange.
-      const context = [];
+      const data = [];
       const expectedError = new Error(
-        "Invalid Argument: context must be type 'object', recieved array",
+        "Invalid Argument: data must be type 'object', recieved array",
       );
 
       // Act.
-      const func = compileTextTemplate.bind(null, text, context);
+      const func = compileTextTemplate.bind(null, text, data);
 
       // Assert.
       expect(func).toThrow(expectedError);
--- apps/notification/src/common/utils/template.utils.ts
+++ apps/notification/src/common/utils/template.utils.ts
@@ -2,40 +2,40 @@ import * as flatten from 'flat';
 
 /**
  * Yields a string where the key pattern(s) have been replaced with the corresponding
- * values from the context. Throws an error if key pattern(s) are present and context
- * is null/undefined or if context is not an object.
+ * values from the data. Throws an error if key pattern(s) are present and data
+ * is null/undefined or if data is not an object.
  * @example
  *  - 'Lorem ipsum dolor {{hello}} amet.';
  *  - 'Lorem ipsum dolor {{ hello.world }} amet.';
  * @param {string} text
- * @param {any} context 
+ * @param {any} data 
  * @returns {string}
  */
-export function compileTextTemplate(text: string, context: any): string {
+export function compileTextTemplate(text: string, data: any): string {
   const regexPattern = /\{\{[\s\S][a-zA-Z0-1\.]+[\s\S]\}\}/g;
 
   if (!regexPattern.test(text)) {
     return text;
   }
 
-  if (!context) {
+  if (!data) {
     throw new Error(
-      'Invalid Argument: context cannot be null/undefined if the key pattern is present',
+      'Invalid Argument: data cannot be null/undefined if the key pattern is present',
     );
   }
 
-  if (typeof context !== 'object' || Array.isArray(context)) {
+  if (typeof data !== 'object' || Array.isArray(data)) {
     throw new Error(
-      `Invalid Argument: context must be type 'object', recieved ${
-        Array.isArray(context) ? 'array' : typeof context
+      `Invalid Argument: data must be type 'object', recieved ${
+        Array.isArray(data) ? 'array' : typeof data
       }`,
     );
   }
 
-  const flatContext = flatten(context);
+  const flatData = flatten(data);
 
   return text.replace(regexPattern, (match) => {
     const key = match.replace(/^\{\{|\}\}$/g, '').trim();
-    return flatContext[key];
+    return flatData[key];
   });
 }
