fd2dfc44e88c64ba9e96222cefc808533fb437b1
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-04-25 06:05:27 -0500

    Message Service Unit Tests & Comments

--- apps/distribution/src/resources/message/message.service.ts
+++ apps/distribution/src/resources/message/message.service.ts
@@ -7,6 +7,11 @@ import { CreateMessageDto } from './dto/create-message.dto';
 export class MessageService {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
+  /**
+   * Publishes a message to an exchange.
+   * @param {CreateMessageDto} createMessageDto
+   * @returns {Promise<ApiResponseDto>}
+   */
   async create({ exchange, routingKey, message }: CreateMessageDto) {
     await this.amqpConnection.publish(exchange, routingKey, message);
     return new ApiResponseDto(`Successfully sent message to ${exchange}!`);
