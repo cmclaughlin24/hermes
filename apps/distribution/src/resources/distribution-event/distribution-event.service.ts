dd1b3f8dd93a9f41b65427d49ddc8352496482d7
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-05-10 10:44:39 -0500

    Distribution Event | Service Comments + Working Update

--- apps/distribution/src/resources/distribution-event/distribution-event.service.ts
+++ apps/distribution/src/resources/distribution-event/distribution-event.service.ts
@@ -6,6 +6,8 @@ import {
 import { InjectModel } from '@nestjs/sequelize';
 import { ApiResponseDto } from '@notification/common';
 import * as _ from 'lodash';
+import { DistributionRule } from '../distribution-rule/entities/distribution-rule.entity';
+import { Subscription } from '../subscription/entities/subscription.entity';
 import { CreateDistributionEventDto } from './dto/create-distribution-event.dto';
 import { UpdateDistributionEventDto } from './dto/update-distribution-event.dto';
 import { DistributionEvent } from './entities/distribution-event.entity';
@@ -17,8 +19,17 @@ export class DistributionEventService {
     private readonly distributionEventModel: typeof DistributionEvent,
   ) {}
 
+  /**
+   * Yields a list of DistributionEvents or throws a NotFoundException if the
+   * repository return null, undefined, or an empty list.
+   * @param {boolean} includeRules
+   * @param {boolean} includeSubscriptions
+   * @returns {Promise<DistributionEvent[]>}
+   */
   async findAll(includeRules: boolean, includeSubscriptions: boolean) {
-    const distributionEvents = await this.distributionEventModel.findAll();
+    const distributionEvents = await this.distributionEventModel.findAll({
+      include: this._buildIncludes(includeRules, includeSubscriptions),
+    });
 
     if (_.isEmpty(distributionEvents)) {
       throw new NotFoundException('Distribution events not found!');
@@ -27,6 +38,15 @@ export class DistributionEventService {
     return distributionEvents;
   }
 
+  /**
+   * Yields a DistributionEvent or throws a NotFoundException if the repository
+   * returns null or undefined.
+   * @param {string} queue
+   * @param {string} messageType
+   * @param {boolean} includeRules
+   * @param {boolean} includeSubscriptions
+   * @returns {Promise<DistributionEvent>}
+   */
   async findOne(
     queue: string,
     messageType: string,
@@ -35,6 +55,7 @@ export class DistributionEventService {
   ) {
     const distributionEvent = await this.distributionEventModel.findOne({
       where: { queue, messageType },
+      include: this._buildIncludes(includeRules, includeSubscriptions),
     });
 
     if (!distributionEvent) {
@@ -46,6 +67,12 @@ export class DistributionEventService {
     return distributionEvent;
   }
 
+  /**
+   * Creates a DistributionEvent of throws a BadRequestException if a distribution event
+   * for a queue and messageType exists in the repository.
+   * @param {CreateDistributionEventDto} createDistributionEventDto
+   * @returns {Promise<ApiResponseDto<DistributionEvent>>}
+   */
   async create(createDistributionEventDto: CreateDistributionEventDto) {
     const existingEvent = await this.distributionEventModel.findOne({
       where: {
@@ -70,12 +97,20 @@ export class DistributionEventService {
     );
   }
 
+  /**
+   * Updates a DistributionEvent or throws a NotFoundException if the repository
+   * returns null or undefined.
+   * @param {string} queue
+   * @param {string} messageType
+   * @param {UpdateDistributionEventDto} updateDistributionEventDto
+   * @returns {Promise<ApiResponseDto<DistributionEvent>>}
+   */
   async update(
     queue: string,
     messageType: string,
     updateDistributionEventDto: UpdateDistributionEventDto,
   ) {
-    const distributionEvent = await this.distributionEventModel.findOne({
+    let distributionEvent = await this.distributionEventModel.findOne({
       where: { queue, messageType },
     });
 
@@ -84,8 +119,26 @@ export class DistributionEventService {
         `Distribution Event for queue=${queue} messageType=${messageType} not found!`,
       );
     }
+
+    distributionEvent = await distributionEvent.update({
+      metadataLabels:
+        updateDistributionEventDto.metadataLabels ??
+        distributionEvent.metadataLabels,
+    });
+
+    return new ApiResponseDto<DistributionEvent>(
+      `Successfully updated distribution event for queue=${queue} messageType=${messageType}!`,
+      distributionEvent,
+    );
   }
 
+  /**
+   * Removes a DistributionEvent or throws a NotFoundException if the repository
+   * returns null or undefined.
+   * @param {string} queue
+   * @param {string} messageType
+   * @returns {Promise<ApiResponseDto>}
+   */
   async remove(queue: string, messageType: string) {
     const distributionEvent = await this.distributionEventModel.findOne({
       where: { queue, messageType },
@@ -102,5 +155,26 @@ export class DistributionEventService {
     return new ApiResponseDto(
       `Successfully deleted distribution event for queue=${queue} messageType=${messageType}!`,
     );
+  }
+
+  /**
+   * Yields a list of objects containing the related models (DistributionRules and/or Subscriptions)
+   * to be included on a DistributionEvent repository query.
+   * @param {boolean} includeRules
+   * @param {boolean} includeSubscriptions
+   * @returns
+   */
+  private _buildIncludes(includeRules: boolean, includeSubscriptions: boolean) {
+    const includes = [];
+
+    if (includeRules) {
+      includes.push({ model: DistributionRule });
+    }
+
+    if (includeSubscriptions) {
+      includes.push({ model: Subscription });
+    }
+
+    return includes;
   }
 }
