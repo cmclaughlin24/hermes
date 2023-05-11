c7152e7710bd87c075d7820199ea94c7bfa5e90b
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-05-11 14:05:47 -0500

    CreateSubscriptionDto Swagger Documentation

--- apps/distribution/src/resources/subscription/dto/create-subscription.dto.ts
+++ apps/distribution/src/resources/subscription/dto/create-subscription.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform, TransformFnParams, Type } from 'class-transformer';
 import {
   IsEnum,
@@ -5,28 +6,42 @@ import {
   IsOptional,
   IsString,
   IsUUID,
-  ValidateNested
+  ValidateNested,
 } from 'class-validator';
 import { DistributionEventExists } from '../../../common/decorators/distribution-event-exists.decorator';
 import { FilterJoinOps } from '../../../common/types/filter.types';
 import { SubscriptionFilterDto } from './subscription-filter.dto';
 
 export class CreateSubscriptionDto {
+  @ApiProperty({
+    description: 'Universal Unique Identifier (UUID) to identify the subscription'
+  })
   @IsUUID('4')
   id: string;
 
+  @ApiProperty({
+    description: 'Name of the Rabbitmq queue the message is consumed from',
+  })
   @IsString()
   queue: string;
 
+  @ApiProperty({
+    description: 'Name of the message (event) the subscripton applies to',
+  })
   @IsString()
   @IsNotEmpty()
   @DistributionEventExists()
   @Transform(({ value }: TransformFnParams) => value?.trim())
   messageType: string;
 
+  @ApiProperty({})
   @IsString()
   url: string;
 
+  @ApiProperty({
+    description: 'Method used to join the subscription filter(s)',
+    enum: FilterJoinOps,
+  })
   @IsEnum(FilterJoinOps)
   filterJoin: FilterJoinOps;
 
