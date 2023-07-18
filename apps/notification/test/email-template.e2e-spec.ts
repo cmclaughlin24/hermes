78f593bb953bcd9a4311c394fb93caf63b155f8b
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-07-18 14:58:58 -0500

    Email Template E2E Tests Pt. 1

--- apps/notification/test/email-template.e2e-spec.ts
+++ apps/notification/test/email-template.e2e-spec.ts
@@ -1,7 +1,12 @@
-import { HttpServer, INestApplication } from '@nestjs/common';
+import { ApiKeyGuard } from '@hermes/common';
+import { HttpServer, HttpStatus, INestApplication } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { useGlobalPipes } from '../src/config/use-global.config';
+import { CreateEmailTemplateDto } from '../src/resources/email-template/dto/create-email-template.dto';
 import { EmailTemplateModule } from '../src/resources/email-template/email-template.module';
 
 describe('[Feature] Email Template', () => {
@@ -12,6 +17,7 @@ describe('[Feature] Email Template', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
+          isGlobal: true,
           envFilePath: `${process.cwd()}/env/notification.env`,
         }),
         SequelizeModule.forRootAsync({
@@ -31,9 +37,11 @@ describe('[Feature] Email Template', () => {
         }),
         EmailTemplateModule,
       ],
+      providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    useGlobalPipes(app);
     await app.init();
     httpServer = app.getHttpServer();
   });
@@ -42,13 +50,109 @@ describe('[Feature] Email Template', () => {
     await app.close();
   });
 
-  it.todo('Create Email Template [POST /]');
+  describe('Create Email Template [POST /]', () => {
+    it('should respond with a CREATED status if the resource was created', () => {
+      // Arrange.
+      const createEmailTemplateDto: CreateEmailTemplateDto = {
+        name: 'e2e-test',
+        subject: 'E2E Testing',
+        template: '<h1></h1>',
+        context: {},
+      };
 
-  it.todo('Get Email Templates [GET /]');
+      // Act/Assert.
+      return request(httpServer)
+        .post('/email-template')
+        .set(process.env.API_KEY_HEADER, process.env.API_KEY)
+        .send(createEmailTemplateDto)
+        .expect(HttpStatus.CREATED);
+    });
 
-  it.todo('Get Email Template [GET /:name]');
+    it('should respond with a BAD_REQUEST status if the payload is invalid', () => {
+      // Arrange.
+      const createEmailTemplateDto = {
+        name: 'e2e-test',
+        subject: 'E2E Testing',
+        context: null,
+      };
 
-  it.todo('Update Email Template [PATCH /:name]');
+      // Act/Assert.
+      return request(httpServer)
+        .post('/email-template')
+        .set(process.env.API_KEY_HEADER, process.env.API_KEY)
+        .send(createEmailTemplateDto)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
 
-  it.todo('Remove Email Template [DELETE /:name]');
+    it('should respond with a FORBIDDEN status if the request is not authorized', () => {
+      // Arrange.
+      const createEmailTemplateDto: CreateEmailTemplateDto = {
+        name: 'e2e-test',
+        subject: 'E2E Testing',
+        template: '<h1></h1>',
+        context: null,
+      };
+
+      // Act/Assert.
+      return request(httpServer)
+        .post('/email-template')
+        .send(createEmailTemplateDto)
+        .expect(HttpStatus.FORBIDDEN);
+    });
+  });
+
+  describe('Get Email Templates [GET /]', () => {
+    it('should respond with an OK status if resource(s) were found', () => {
+      // Act/Assert.
+      return request(httpServer).get('/email-template').expect(HttpStatus.OK);
+    });
+
+    it.todo(
+      'should respond with a NOT_FOUND status if resource(s) were not found',
+    );
+  });
+
+  describe('Get Email Template [GET /:name]', () => {
+    it('should respond with an OK status if the resource exists', () => {
+      // Act/Assert.
+      return request(httpServer)
+        .get(`/email-template/e2e-test`)
+        .expect(HttpStatus.OK);
+    });
+
+    it('should respond with a NOT_FOUND status if the resource does not exist', () => {
+      // Act/Assert.
+      return request(httpServer)
+        .get(`/email-template/unit-test`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('Update Email Template [PATCH /:name]', () => {
+    it.todo('should respond with an OK status if the resource was updated');
+
+    it.todo(
+      'should respond with a BAD_REQUEST status if the payload is invalid',
+    );
+
+    it.todo(
+      'should respond with a FORBIDDEN status if the request is not authorized',
+    );
+
+    it.todo(
+      'should respond with a NOT_FOUND status if the resource does not exist',
+    );
+  });
+
+  describe('Remove Email Template [DELETE /:name]', () => {
+    it.todo('should respond with an OK status if the resource was deleted');
+
+    it.todo(
+      'should respond with a FORBIDDEN status if the request is not authorized',
+    );
+
+    it.todo(
+      'should respond with a NOT_FOUND status if the resource does not exist',
+    );
+  });
 });
