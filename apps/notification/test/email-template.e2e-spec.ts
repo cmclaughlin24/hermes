d6d7b1a38110a7aca0860449a7925a3c5f25d61b
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-07-18 07:34:49 -0500

    E2E Test Set-up

--- apps/notification/test/email-template.e2e-spec.ts
+++ apps/notification/test/email-template.e2e-spec.ts
@@ -2,10 +2,7 @@ import { HttpServer, INestApplication } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
-import { databaseFactory } from '../src/config/database.config';
 import { EmailTemplateModule } from '../src/resources/email-template/email-template.module';
-
-console.log(`${process.cwd()}/env/notification.env`)
 
 describe('[Feature] Email Template', () => {
   let app: INestApplication;
@@ -15,13 +12,22 @@ describe('[Feature] Email Template', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
-          isGlobal: true,
           envFilePath: `${process.cwd()}/env/notification.env`,
         }),
         SequelizeModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],
-          useFactory: databaseFactory,
+          useFactory: (configService: ConfigService) => ({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_NAME'),
+            autoLoadModels: true,
+            synchronize: true,
+            logging: false,
+          }),
         }),
         EmailTemplateModule,
       ],
