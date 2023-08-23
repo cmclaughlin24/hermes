cb163cba1b5a83181007e19580cdbfc956cbedea
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-08-23 05:26:35 -0500

    Add descriptions to custom exceptions

--- libs/common/src/errors/exists.exception.ts
+++ libs/common/src/errors/exists.exception.ts
@@ -1,1 +1,5 @@
+/**
+ * This custom exception class is intended to be thrown when non-null
+ * value is encountered in a situation where null/undefined was expected.
+ */
 export class ExistsException extends Error {}
--- libs/common/src/errors/missing.exception.ts
+++ libs/common/src/errors/missing.exception.ts
@@ -1,1 +1,5 @@
+/**
+ * This custom exception class is designed to be thrown when an
+ * expected value is not found during the execution of an application. 
+ */
 export class MissingException extends Error {}
