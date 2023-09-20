70520a1a54877711e928b9ff4ac7d546a5a95b89
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-09-19 19:41:02 -0500

    Add DevtoolsModule and OpenTelemetryModule

--- apps/iam/src/app.module.ts
+++ apps/iam/src/app.module.ts
@@ -1,8 +1,35 @@
+import { OpenTelemetryModule } from '@hermes/open-telemetry';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      envFilePath: `${process.cwd()}/env/iam.env`,
+      validationSchema: Joi.object({
+        ENABLE_DEVTOOLS: Joi.boolean().default(false),
+        DEVTOOLS_PORT: Joi.number().default(8002),
+        ENABLE_OPEN_TELEMETRY: Joi.boolean().default(false),
+      }),
+    }),
+    OpenTelemetryModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        enableOpenTelemetry: configService.get('ENABLE_OPEN_TELEMETRY'),
+      }),
+    }),
+    DevtoolsModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        http: configService.get('ENABLE_DEVTOOLS'),
+        port: configService.get('DEVTOOLS_PORT'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
