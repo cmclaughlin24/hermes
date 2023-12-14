45b419fec7a98fa1eefdad7f0174ba8aff55bfa4
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-12-14 13:07:38 -0600

    Add documentation to delivery window entity

--- apps/iam/src/resources/user/entities/delivery-window.entity.ts
+++ apps/iam/src/resources/user/entities/delivery-window.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import {
   Column,
   Entity,
@@ -13,18 +13,44 @@ import { User } from './user.entity';
 @ObjectType()
 export class DeliveryWindow {
   @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID, {
+    description:
+      'A universial unique identifier (UUID) that identifies a unique ' +
+      'delivery window. Utilized to allow a user to specify multiple delivery ' +
+      'windows on the same day of the week.',
+  })
   id: string;
 
   @Column()
+  @Field({
+    description:
+      'An integer, between 0 and 6, representing the day of the week where ' +
+      'Sunday is 0 and Saturday is 6.',
+  })
   dayOfWeek: number;
 
   @Column()
+  @Field({
+    description:
+      'An integer, between 0 and 23, representing the hour at which the ' +
+      'delivery window starts.',
+  })
   atHour: number;
 
   @Column()
+  @Field({
+    description:
+      'An integer, between 0 and 59, representing the minutes past the hour ' +
+      'the delivery window starts.',
+  })
   atMinute: number;
 
   @Column()
+  @Field({
+    description:
+      'An integer, greater than 0, representing the length of the delivery ' +
+      'window in minutes.',
+  })
   duration: number;
 
   @ManyToOne(() => User, (user) => user.deliveryWindows, {
