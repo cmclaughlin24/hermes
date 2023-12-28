a7e5c041a40b0892b5c92ea5c93c580cc84b1bfe
cmclaughlin24 <curtismclaughlin91@gmail.com>
2023-12-28 11:21:37 -0600

    Test custom action

--- .github/actions/npm-package-version/main.js
+++ .github/actions/npm-package-version/main.js
@@ -1,10 +1,41 @@
 const core = require('@actions/core');
+const path = require('path');
+
+/**
+ * Yields a boolean indicating if the file extension is `.json`.
+ * @param {string} filePath
+ * @returns {boolean}
+ */
+function isJsonFile(filePath) {
+  return path.extname(filePath) === '.json';
+}
 
 function main() {
   const filePath = core.getInput('filePath');
-  console.log(filePath);
+
+  if (!isJsonFile(filePath)) {
+    throw new Error(
+      `Invalid file extension: ${filePath} does not match extension .json`,
+    );
+  }
+
   const packageJson = require(filePath);
-  console.log(packageJson);
+
+  if (!packageJson || Object.keys(packageJson).length === 0) {
+    throw new Error(
+      `Invalid package.json file: package.json file appears to be empty`,
+    );
+  }
+
+  const version = packageJson['version'];
+
+  if (!version || version.trim() === '') {
+    throw new Error(
+      'Invalid package.json version: version does not exist or is an empty string',
+    );
+  }
+
+  core.setOutput('version', version);
 }
 
 main();
