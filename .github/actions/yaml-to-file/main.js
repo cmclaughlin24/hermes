bb3d2a868019a9a6d3a3bd7e6e4a660e005a9ea5
cmclaughlin24 <curtismclaughlin91@gmail.com>
2024-01-05 05:45:23 -0600

    Add yaml-to-file custom action

---
+++ .github/actions/yaml-to-file/main.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const { createHash } = require('crypto');
+
+async function createFileName(content, extension = 'yaml') {
+  return new Promise((resolve, reject) => {
+    const hash = createHash('sha256');
+
+    hash.on('readable', () => {
+      const data = hash.read();
+
+      if (!data) {
+        reject('Failed to generate file hash, null was read from buffer');
+      }
+
+      resolve(`${data}.${extension}`);
+    });
+
+    hash.write(content);
+    hash.end();
+  });
+}
+
+async function main() {
+  const content = core.getInput('content', { required: true });
+  let fileName = core.getInput('fileName');
+
+  if (content.trim() === '') {
+    throw new Error('Invalid input: content cannot be an empty string');
+  }
+
+  if (!fileName || fileName.trim() === '') {
+    core.info('fileName is not present, generating file name hash');
+
+    fileName = await createFileName(content);
+
+    core.info(`fileName: ${content}`);
+  }
+
+  console.log(content)
+}
+
+await main();
